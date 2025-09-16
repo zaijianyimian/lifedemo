0c90d3375b0b08ea7451f3d0cf6a23a30d83fab5
zaijianyimian <guoqiming6661@gmail.com>
2025-09-16 08:44:09 +0800

    对界面布局进行了一定修改

--- Admin/src/main/java/com/example/admin/AdminApplication.java
+++ Admin/src/main/java/com/example/admin/AdminApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
 @MapperScan("com.example.admin.mapper")
 public class AdminApplication {
 
